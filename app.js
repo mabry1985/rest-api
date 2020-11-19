34738efc5be57c956233a60d85257d9b326c5b0b
Josh Mabry <mabry1985@gmail.com>
2020-11-19 12:39:43 -0800

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/feed', feedRoutes);
 
