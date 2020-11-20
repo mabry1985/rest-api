56d9a222418effed48bdaa8e230070aabd6f715c
Josh Mabry <mabry1985@gmail.com>
2020-11-20 09:24:09 -0800

    handle errors for create post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Josh",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation failed, entered data is incorrect",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
 
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Josh" },
+      createdAt: new Date(),
+    },
   });
 };
